78f497334c1bed6df8019454783c6fe9acdceb28
Daria Filinkova <daria.filinkova@gmail.com>
2021-12-23 23:56:02 +0300

    Small fixes

--- src/test/java/KayakFlightPageTest.java
+++ src/test/java/KayakFlightPageTest.java
@@ -2,17 +2,13 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import page.KayakFlightsPage;
 
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.empty;
-
 public class KayakFlightPageTest extends CommonConditions {
 
     private final String EXPECTED_DESTINATION = "Paris, France (PAR)";
     private final String EXPECTED_ERROR_MESSAGE = "Searches need at least 1 traveler";
     private final String EXPECTED_ERROR_MESSAGE_FRAGMENT = "Please enter unique 'From' and 'To' airports.";
 
-    //@Test //must be the 1st
+    @Test
     public void testChangeDestinations() {
         KayakFlightsPage flightsPage = new KayakFlightsPage(driver)
                 .openPage()
@@ -22,7 +18,7 @@ public class KayakFlightPageTest extends CommonConditions {
                 EXPECTED_DESTINATION);
     }
 
-    //@Test
+    @Test
     public void testNullNumberOfAdults() {
         KayakFlightsPage flightsPage = new KayakFlightsPage(driver)
                 .openPage()
@@ -32,7 +28,7 @@ public class KayakFlightPageTest extends CommonConditions {
                 EXPECTED_ERROR_MESSAGE);
     }
 
-    @Test
+    //@Test
     public void testSameInputDestinations() throws InterruptedException {
         KayakFlightsPage flightsPage = new KayakFlightsPage(driver)
                 .openPage()
@@ -41,7 +37,5 @@ public class KayakFlightPageTest extends CommonConditions {
         Thread.sleep(1000);
         Assert.assertEquals(flightsPage.getTextFromErrorFragment(),
                 EXPECTED_ERROR_MESSAGE_FRAGMENT);
-        //Assert.assertTrue(flightsPage.appearErrorFragment());
-
     }
 }
